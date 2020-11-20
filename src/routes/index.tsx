556905e0cc13eb0a9c2588faad1e521af150349e
DaniloSVJ <danilocotatoceu@gmail.com>
2020-11-20 13:07:56 -0300

    react js first project

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
+// <Route path="/repositories" component={Repository} />
 import Dashboard from '../pages/Dashboard/index';
 import Repository from '../pages/Repository/index';
 // Switch vai garantir que apenas uma rota seja visivel
 const Routes: React.FC = () => (
     <Switch>
         <Route path="/" exact component={Dashboard} />
-        <Route path="/repositories" component={Repository} />
-        <Route path="repositories/:repository+" component={Repository} />
+
+        <Route path="/repositories/:repository+" component={Repository} />
     </Switch>
 );
 
